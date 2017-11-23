a3f93e3528bea513329a09f28e06d6cc291fb617
ashin-person <1123842546@qq.com>
2017-11-23 23:59:06 +0800

    11

---
+++ src/main/java/ljx/ashin/distribute/ZKDistributeServer.java
@@ -0,0 +1,87 @@
+package ljx.ashin.distribute;
+
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+
+/**
+ * zookeeper分布式服务端
+ * Created by AshinLiang on 2017/11/23.
+ */
+public class ZKDistributeServer {
+
+    private static final String connectString = "192.168.152.129:2181";//连接串
+    private static final int sessionTimeout = 30000;//超时设置
+
+    private String parentNode = "/servers";//父节点
+
+    private ZooKeeper zk = null;
+
+    /**
+     * 连接上ZK
+     */
+    private void connectZK(){
+        try {
+            zk = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
+                public void process(WatchedEvent watchedEvent) {
+                    //处理函数
+                    System.out.println("事件类型："+watchedEvent.getType()+" 路径"+watchedEvent.getPath());
+                    try {
+                        zk.getChildren("/",true);
+                    } catch (KeeperException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 向服务器注册节点
+     * @param node
+     * @return
+     */
+    private String registerServer(String node){
+        String nodePath = null;
+        try {
+                //判断父节点是否存在，不存在则创建
+               if (null==zk.exists(parentNode,false)){
+                   zk.create(parentNode,"parentNodeValue".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
+               }
+               nodePath = zk.create(parentNode+"/"+node,node.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
+            System.out.println("注册的节点路径为："+nodePath);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return nodePath;
+    }
+
+    /**
+     * 业务处理
+     * @param node
+     */
+    public void proccessBussiness(String node){
+        System.out.println("服务器:"+node+"已经启动。。。");
+        System.out.println("进行业务处理");
+        try {
+            Thread.sleep(Long.MAX_VALUE);//睡眠，防止线程结束
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String param = args[0];//输入的第一个参数
+        ZKDistributeServer zkDistributeServer = new ZKDistributeServer();
+        zkDistributeServer.connectZK();
+        zkDistributeServer.registerServer(param);
+        zkDistributeServer.proccessBussiness(param);
+    }
+}
---
+++ src/main/java/ljx/ashin/service/ZkDemo.java
@@ -0,0 +1,111 @@
+package ljx.ashin.service;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by AshinLiang on 2017/11/18.
+ */
+
+public class ZkDemo {
+
+    private ZooKeeper zk = null;
+
+    /**
+     * 初始化zookeeper客户端
+     */
+    @Before
+    public void init(){
+        String connectString = "192.168.152.129:2181";//连接窜
+        int sessionTimeout = 30000;
+        try {
+            System.out.println("==开始初始化zookeeper==");
+            zk = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
+                public void process(WatchedEvent watchedEvent) {
+                    System.out.println("触发了事件类型:"+watchedEvent.getType()+"事件的路径:"+watchedEvent.getPath());
+                    try {
+                        zk.getChildren("/",true);
+                    } catch  (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建节点
+     */
+    @Test
+    public void testCreateZkNode(){
+        if(zk!=null){
+            try {
+                String data = "zkSum";
+                String path = zk.create("/ashinA",data.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+                System.out.println("在"+path+"中添加了数据:"+data);
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }else {
+            System.out.println("zookeeper为null");
+        }
+    }
+
+    /**
+     * 获取子节点
+     */
+    @Test
+    public void testGetChildren(){
+        try {
+            System.out.println("调用方法获取子节点");
+            List<String> childrens = zk.getChildren("/",true);
+            for (String child:childrens) {
+                System.out.println("子节点："+child);
+            }
+            Thread.sleep(Integer.MAX_VALUE);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除节点
+     */
+    @Test
+    public void testDeleteNode(){
+        try {
+            zk.delete("/ashinZK0000000002",-1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 检查节点是否存在
+     */
+    @Test
+    public void testExistNode(){
+        try {
+            Stat stat = zk.exists("/ashinB",false);
+            System.out.println(stat);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
